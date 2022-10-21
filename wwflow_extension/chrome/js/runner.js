92534d0f30ec38dcefd63c83a655214cb43e12c7
William Qin <william.qin51@gmail.com>
2022-10-20 20:14:24 -0400

    some html injection

--- wwflow_extension/chrome/js/runner.js
+++ wwflow_extension/chrome/js/runner.js
@@ -1,4 +1,5 @@
 const postings = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm"
+const dashboard = "https://waterlooworks.uwaterloo.ca/myAccount/dashboard.htm"
 
 var searchAction = "";
 
@@ -160,5 +161,14 @@ chrome.storage.session.onChanged.addListener(function (changes) {
     const state = changes["state"];
     if (state !== undefined) {
         main();
+    }
+});
+
+$.get(chrome.runtime.getURL('html/ww_helper.html'), function(data) {
+    $($.parseHTML(data)).prependTo('main');
+    if (window.location.href === dashboard) {
+        $.get(chrome.runtime.getURL('html/ww_dashboard.html'), function(data_content) {
+            $($.parseHTML(data_content)).prependTo('#ck_content_container');
+        });
     }
 });
