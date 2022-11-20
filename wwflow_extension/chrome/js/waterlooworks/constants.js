eafd28492f79662d42b3e06ac724aa87ad10506a
William Qin <william.qin51@gmail.com>
2022-11-20 14:11:17 -0500

    job page now has ck_jobid query param

--- wwflow_extension/chrome/js/waterlooworks/constants.js
+++ wwflow_extension/chrome/js/waterlooworks/constants.js
@@ -1,2 +1,6 @@
 const postings = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.htm"
 const dashboard = "https://waterlooworks.uwaterloo.ca/myAccount/dashboard.htm"
+
+const queryParamJobId = "ck_jobid"
+const queryParamCommand = "ck_cmd"
+const queryParamRedirect = "ck_redirect"
