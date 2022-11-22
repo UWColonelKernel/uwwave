39b43e444ea074d67caed384771bae33f4900f2a
linda9379 <linda051011@gmail.com>
2022-11-21 21:22:12 -0500

    fixed imports

--- frontend/src/components/JobsPage.js
+++ frontend/src/components/JobsPage.js
@@ -1,13 +1,5 @@
 import React from 'react'
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import {Box, Button} from '@mui/material';
+import {Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow} from '@mui/material';
 import { Color } from "../styles/color";
 import SearchBar from './SearchBar';
 
