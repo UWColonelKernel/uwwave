cc77905a543de11fc69b9e4057cceae93fb8c5ec
unknown <bryanlin107@gmail.com>
2022-11-25 16:29:25 -0500

    Search bar component

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,16 +1,59 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Container from '@mui/material/Container';
-import SearchBar from "../SearchBar"
+import SearchBar from "../SearchBar/SearchBar"
 import styled from "styled-components";
 import { Spacer } from "../Spacer/Spacer";
 import { Typography }from '../MUI/Typography';
 import { Color } from "../../styles/color";
 import { Tiles } from "./Tiles/Tiles";
 
+//EXMAPLE OF SEARCH BAR COMPONENT PROPS
+//TODO: Replace
+const DUMMY_ITEMS = [
+  {
+    title: "Microsoft"
+  },
+  {
+    title: "Apple"
+  },
+  {
+    title: "Mcdonalds"
+  },
+]
+
+const DummyListItem = (props) => {
+  const {
+    title
+  } = props;
+  return (
+    <ListItemWrapper>
+      <Typography>{title}</Typography>
+    </ListItemWrapper>
+  )
+}
+
+const ListItemWrapper = styled.div`
+  width: 100%;
+  padding: 16px;
+  display: flex;
+  background-color: white;
+`
+//END OF DUMMY SEARCH BAR PROPS
+
 export default function HomePage() {
   useEffect(() => {
     document.title = 'UW Wave';
   }, []);
+
+  const [searchListItems, setSearchListItems] = useState([]);
+
+  const onSearchValChange = (val) => {
+    if(val){
+      setSearchListItems(DUMMY_ITEMS.filter(item=>item.title.toUpperCase().includes(val.toUpperCase())))
+    }else{
+      setSearchListItems([]);
+    }
+  }
 
   return (
     <>
@@ -20,7 +63,12 @@ export default function HomePage() {
         Search on UW Wave to find Jobs, Companies, and Reviews
       </Typography>
       <Spacer height={32}/>
-      <SearchBar width="50%" minWidth="300px"/>
+      <SearchBar
+        width="50%"
+        listItems={searchListItems}
+        Component={DummyListItem}
+        onSearchValChange={onSearchValChange}
+      />
     </Hero>
     </HeroWrapper>
     <Tiles/>
@@ -39,10 +87,7 @@ const Hero = styled(Container)`
     min-height: 400px;
   }
 `
-
 const HeroWrapper = styled.div`
   width: 100%;
   background-color: #F0F9FF;
 `
-
-
--- frontend/src/views/Page.js
+++ frontend/src/views/Page.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState }from "react";
 import { useParams } from "react-router-dom";
 import { convertRawJobForJobPage } from "util/extension_adapter";
 import { buildExtensionApiListener } from "util/extension_api";
