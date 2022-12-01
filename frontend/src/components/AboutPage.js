dae7571563a54fa8331d7c521cf10f067834ec56
William Qin <william.qin51@gmail.com>
2022-11-30 21:08:18 -0500

    about page

--- frontend/src/components/AboutPage.js
+++ frontend/src/components/AboutPage.js
@@ -12,10 +12,10 @@ export default function AboutPage() {
       <Row className='justify-content-center' style={{fontSize: '1.5em'}}>
         <Col className='col-10 align-self-center'>
           <h1 className='text-primary' style={{fontWeight: 'bold'}}> 
-            Hi! We're Team Colonel Kernel!
+            We're Team Colonel Kernel
           </h1>
           <p className='text-primary'> 
-            uwWave is our SE390 project that we're considering to pursue as our Capstone.
+            UW Wave is our SE 390 project that we're considering to pursue as our capstone project.
           </p>
           <p className='text-primary'> 
             Currently, we consist of 5 students in SE24: <br/>
@@ -25,7 +25,7 @@ export default function AboutPage() {
       </Row>
       <Spacer height={32}/>
       <Row className='justify-content-center' style={{fontSize: '1.2em'}}>
-        <Col className='bg-primary text-white col-10'>
+        <Col className='col-10'>
           <h2 style={{fontWeight: "bold"}}>Feedback:</h2>
           <p> 
             If you have any feedback, questions, or concerns about Wave, 
