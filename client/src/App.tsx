8e53bd4219b5ce271c2a5c0f4a2f4fa3a5ccd70a
a28dong <andrewdong1@gmail.com>
2023-03-17 21:14:14 -0700

    Set Global Font via theme to Lato

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,33 @@ import { Spacer } from 'components/Spacer/Spacer'
 import { Footer } from 'components/Footer/Footer'
 import { NavigationBar } from 'components/NavigationBar/NavigationBar'
 import { HomePage } from 'views/HomePage/HomePage'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Lato', 'san-serif'].join(','),
+  },
+})
 
 export const App = () => {
   return (
-    <div>
-      <NavigationBar />
-      <div style={{ minHeight: `calc(100vh - 185px)` }}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            {/* <Route path = '/login' element={<LoginPage />} />
-            <Route path = '/jobs' element={<JobsPage />} />
-            <Route path = '/jobs/:jobId' element={<Job/>} />
-            <Route path = '/about-us' element={<AboutPage/>}/> */}
-          </Routes>
-        </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <div>
+        <NavigationBar />
+        <div style={{ minHeight: `calc(100vh - 185px)` }}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              {/* <Route path = '/login' element={<LoginPage />} />
+              <Route path = '/jobs' element={<JobsPage />} />
+              <Route path = '/jobs/:jobId' element={<Job/>} />
+              <Route path = '/about-us' element={<AboutPage/>}/> */}
+            </Routes>
+          </BrowserRouter>
+        </div>
+        <Spacer height={50} />
+        <Footer />
       </div>
-      <Spacer height={50} />
-      <Footer />
-    </div>
+    </ThemeProvider>
   )
 }
