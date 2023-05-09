11c20a92278af43629923710e2bd9fcab75c3552
William Qin <william.qin51@gmail.com>
2023-05-09 17:01:17 -0400

    update job keys

--- client/src/services/extension/jobKeys.ts
+++ client/src/services/extension/jobKeys.ts
@@ -18,7 +18,7 @@ export enum CompanyInfoFields {
   organization = 'Organization',
 }
 
-// As of Nov 20, 2022, these are the only fields
+// As of May 9, 2023, these are the only fields
 export enum AppInfoFields {
   deadline = 'Application Deadline',
   documentsRequired = 'Application Documents Required',
@@ -26,54 +26,31 @@ export enum AppInfoFields {
   method = 'Application Method',
 }
 
-// As of Nov 20, 2022, these are most of the fields
-// The scraper didn't fully scrape properly, so this will need updating - TODO
+// As of May 9, 2023, these are the fields
 export enum JobInfoFieldsCoop {
   additionalInformation = 'Additional Information',
   additionalJobIdentifiers = 'Additional Job Identifiers',
-  affiliation = 'Affiliation',
-  anticipatedStartDate = 'Anticipated start date',
-  category = 'Category',
-  company = 'Company',
-  compensationGroup = 'Compensation Group',
   compensationAndBenefitsInformation = 'Compensation and Benefits Information',
-  currentWorkTerm = 'Current Work Term',
-  department = 'Department',
-  division = 'Division',
-  duration = 'Duration',
   employerInternalJobNumber = 'Employer Internal Job Number',
-  employmentType = 'Employment Type',
-  hiringManagerOrSupervisor = 'Hiring manager/Supervisor',
-  hoursOfWorkBiWeekly = 'Hours of Work (bi-weekly)',
   jobAddressLineOne = 'Job - Address Line One',
   jobAddressLineTwo = 'Job - Address Line Two',
   jobCity = 'Job - City',
   jobCountry = 'Job - Country',
-  jobPostalOrZipCode = 'Job - Postal Code / Zip Code (X#X #X#)',
-  jobProvinceOrState = 'Job - Province / State',
+  jobPostalCode = 'Job - Postal Code / Zip Code (X#X #X#)',
+  jobProvince = 'Job - Province / State',
   jobCategoryNoc = 'Job Category (NOC)',
-  jobClassificationTitle = 'Job Classification Title',
-  jobId = 'Job ID',
   jobLocation = 'Job Location (if exact address unknown or multiple locations)',
   jobResponsibilities = 'Job Responsibilities',
   jobSummary = 'Job Summary',
-  jobTerm = 'Job Term',
   jobTitle = 'Job Title',
   jobType = 'Job Type',
   level = 'Level',
-  location = 'Location',
   numberOfJobOpenings = 'Number of Job Openings',
-  numberOfPositionsOpen = 'Number of Positions Open',
-  organization = 'Organization',
-  positionTitle = 'Position Title',
   region = 'Region',
-  reportsTo = 'Reports to',
   requiredSkills = 'Required Skills',
-  salary = 'Salary',
   specialJobRequirements = 'Special Job Requirements',
   targetedDegreesAndDisciplines = 'Targeted Degrees and Disciplines',
   transportationAndHousing = 'Transportation and Housing',
-  workLocation = 'Work Location',
   workTerm = 'Work Term',
   workTermDuration = 'Work Term Duration',
 }
